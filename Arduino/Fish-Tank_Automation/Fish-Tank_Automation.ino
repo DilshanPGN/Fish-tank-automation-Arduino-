47290f416cdd51caf87967753ac16e7ef5228868
DilshanPGN <73216265+DilshanPGN@users.noreply.github.com>
2021-10-23 12:42:01 +0530

    Completion of the circuit

--- Arduino/Fish-Tank_Automation/Fish-Tank_Automation.ino
+++ Arduino/Fish-Tank_Automation/Fish-Tank_Automation.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal_I2C.h>
 
+LiquidCrystal_I2C lcd(0x20,16,2); 
+
+
+//defining buttons
+
+#define bt_Options   A0
+#define bt_up     A1
+#define bt_down   A2
+
+
+
+void setup()
+{
+  lcd.init(); 
+  lcd.backlight();
+  
+  lcd.print("Hello, world!");
+  delay(1500);
+  lcd.clear();
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+ 
 }
